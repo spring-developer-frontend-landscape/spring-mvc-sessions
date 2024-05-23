ba9e06a0cebed267e8f716d8644f08258718f00a
Dan Vega <danvega@gmail.com>
2024-05-23 11:02:34 -0400

    initial commit

---
+++ src/test/java/dev/danvega/sessions/SessionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.sessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
