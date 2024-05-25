386afda33b89c93d98d757ca36cc8592fda39bb5
Dan Vega <danvega@gmail.com>
2024-05-24 21:34:18 -0400

    Adding an API controller for the vue frontend demo

---
+++ src/main/java/dev/danvega/sessions/session/SessionApiController.java
@@ -0,0 +1,26 @@
+package dev.danvega.sessions.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sessions")
+public class SessionApiController {
+
+    private static final Logger log = LoggerFactory.getLogger(SessionApiController.class);
+    private final SessionRepository repository;
+
+    public SessionApiController(SessionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Session> findAll() {
+        return repository.findAll();
+    }
+}
