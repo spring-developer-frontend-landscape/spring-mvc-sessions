2594502e450503b5184454cac4c62c137553517b
Dan Vega <danvega@gmail.com>
2024-05-24 22:20:06 -0400

    cleaning up the readme to explain its for both mvc and a REST API for the vue frontend.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Spring I/O Sessions
 
-This is a simple Spring MVC application that will display a list of sessions from the conference.
+This is a simple Spring MVC application that will display a list of sessions from the conference. This is both a traditional MVC app with views using Thymeleaf and it also exposes a REST API for a vue frontend. 
 
 https://2024.springio.net/sessions/
--- src/main/java/dev/danvega/sessions/session/SessionApiController.java
+++ src/main/java/dev/danvega/sessions/session/SessionApiController.java
@@ -2,6 +2,7 @@ package dev.danvega.sessions.session;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sessions")
+@CrossOrigin
 public class SessionApiController {
 
     private static final Logger log = LoggerFactory.getLogger(SessionApiController.class);
